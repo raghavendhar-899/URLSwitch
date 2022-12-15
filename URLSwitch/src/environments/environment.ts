6a6f702476a39f1314fe7bcacca62429cd1e84c8
V N Krishna Raghavendhar A <adusumilliraghavendhar2000@gmail.com>
2022-12-15 10:41:07 -0600

    firebase init

---
+++ URLSwitch/src/environments/environment.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  firebase: {
+    projectId: 'urlshortner-8458d',
+    appId: '1:224634933560:web:b804e1a3a52d287cb3b1cd',
+    storageBucket: 'urlshortner-8458d.appspot.com',
+    apiKey: 'AIzaSyDUK310glkDy7AqOkpQxNP63xIqspHE0-E',
+    authDomain: 'urlshortner-8458d.firebaseapp.com',
+    messagingSenderId: '224634933560',
+    measurementId: 'G-4VKRY5V8PJ',
+  },
+    production: false 
+};
