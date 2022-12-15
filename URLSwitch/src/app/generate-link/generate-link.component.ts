9df37142a7bd57cf5c4f30084bf1d8b9e9f780bd
Rishit Pallav <64432673+rishitpallav@users.noreply.github.com>
2022-12-15 11:08:07 -0600

    generateLink init

---
+++ URLSwitch/src/app/generate-link/generate-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-link',
+  templateUrl: './generate-link.component.html',
+  styleUrls: ['./generate-link.component.css']
+})
+export class GenerateLinkComponent {
+
+}
